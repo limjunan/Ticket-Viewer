b1f18a57818f80b2493e883fec190625618fe2eb
limjunan <bryanlim080302@gmail.com>
2021-11-24 16:46:53 +0800

    chore: added login.html

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template
+from flask.templating import render_template
+import requests
 
 app = Flask(__name__)
 
 # main application route
 @app.route("/")
-def init():
+def login():
+    return render_template('login.html')
+
+@app.route("/index")
+def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
