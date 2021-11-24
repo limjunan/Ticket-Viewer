dcee39a0587905f6df377d9437aea39a396ce8a0
limjunan <bryanlim080302@gmail.com>
2021-11-24 21:36:59 +0800

    chore: updated authentication.html to use forms.py

--- app.py
+++ app.py
@@ -2,15 +2,18 @@ from flask import Flask, render_template
 from flask.templating import render_template
 import requests
 
+from forms import AuthenticationForm
+
 app = Flask(__name__)
 
-# secret key
+# secret key for CSRF protection
 app.config['SECRET_KEY'] = 'b8aZENDESKffba7ea03b21aZENDESK93'
 
 # main application route
 @app.route("/")
-def login():
-    return render_template('authenticate.html')
+def authenticate():
+    authenticationForm = AuthenticationForm()
+    return render_template('authenticate.html', form=authenticationForm)
 
 @app.route("/index")
 def index():
