a66ee4b2204802c1946b80d3614e0025322bd488
limjunan <bryanlim080302@gmail.com>
2021-11-24 14:32:32 +0800

    chore: rendered index.html template

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def init():
-    return "<h1>Zendesk Ticket Viewer</h1>"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
