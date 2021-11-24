c2a277301f8d4a6edbe1597ce537dd62858f9165
limjunan <bryanlim080302@gmail.com>
2021-11-24 21:15:34 +0800

    chore: added forms.py

---
+++ forms.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+
+class AuthenticationForm(FlaskForm):
+    client_id = StringField('Client ID', validators=[DataRequired()])
+    client_secret = PasswordField('Client Secret', validators=[DataRequired()])
+    submit = SubmitField('Authenticate')
