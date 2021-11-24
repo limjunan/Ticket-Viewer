41fa98b4e95aafde525f4b1985a0b632c32ec2ba
limjunan <bryanlim080302@gmail.com>
2021-11-24 23:37:07 +0800

    chore: added authentication logic (not working)

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, redirect
+from os import access
+from flask import Flask, render_template, redirect, request
 from user import User
-import requests
+import requests, json
 
 from forms import AuthenticationForm
 
@@ -18,7 +19,32 @@ def authenticate():
         user = User(authenticationForm.client_id.data, authenticationForm.client_secret.data, 
                     authenticationForm.client_url.data)
         
-        redirect(f'https://{user.client_url}.zendesk.com/oauth/authorizations/new?response_type=code&redirect_uri=http://127.0.0.1:5000&client_id={user.client_id}&scope=read')
+        # get oauth code
+        redirect_url = f'https://{ user.client_url }.zendesk.com/oauth/authorizations/new?response_type=code&redirect_uri=http://127.0.0.1:5000&client_id={ user.client_id }&scope=read'
+        redirect(redirect_url)
+        code = request.args.get('code')
+        print('             ',code)
+        # get oauth access token
+        redirect_url = f'https://{ user.client_url }.zendesk.com/oauth/tokens'
+        headers={ 'Content-Type': 'application/json'}
+        data={"grant_type": "authorization_code",
+            "code": code,
+            "client_id": user.client_id,
+            "client_secret": user.client_secret,
+            "redirect_uri": "http://127.0.0.1:5000",
+            "scope": "read" }
+
+        accessToken = requests.post(redirect_url, headers=headers, data=json.dumps(data)).json()
+        print('                 ',accessToken)
+        access_token = accessToken['access_token']
+
+        # get tickets information json
+        redirect_url = f'https://{ user.client_url }.zendesk.com/api/v2/tickets.json'
+        headers={'Authorization': f'Bearer {access_token}'}
+        tickets = requests.get(redirect_url, headers=headers)
+
+        print(tickets)
+
 
     return render_template('authenticate.html', form=authenticationForm, title='Authentication')
 
