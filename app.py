fbe6181e872dc7533ab28111ea83698475ffbc19
limjunan <bryanlim080302@gmail.com>
2021-11-26 14:26:53 +0800

    refactor: moved index.html to its own route

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from os import access
 from urllib import parse
-from flask import Flask, render_template, redirect, request, session, flash
+from flask import Flask, render_template, redirect, request, session, flash, url_for
 from forms import AuthenticationForm
 from api_auth import *
 from ticket_handler import *
@@ -33,13 +33,18 @@ def authenticate():
         accessTokenJSON = accessToken.json()
         session['access_token'] = accessTokenJSON['access_token']
         if accessToken.status_code == 200:
-            tickets = getTickets(session['access_token'], session['client_url'])
-            return render_template('index.html', title='Index', tickets=tickets)
+            return(redirect(url_for('listTickets')))
+            
         else:
             flash('ERROR', 'error')
-            return render_template('authenticate.html', form=AuthenticationForm(), title='Authentication')
+            return render_template(url_for('authenticate'), form=AuthenticationForm(), title='Authentication')
 
     return render_template('authenticate.html', form=authenticationForm, title='Authentication')
+
+@app.route('/index')
+def listTickets():
+    tickets = getTickets(session['access_token'], session['client_url'])
+    return render_template('index.html', title='Index', tickets=tickets)
 
 @app.route('/ticket', methods=['GET', 'POST'])
 def displayTicket():
