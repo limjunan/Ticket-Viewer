d8bb2d76578232432707d454092c59a530c190bf
limjunan <bryanlim080302@gmail.com>
2021-11-28 14:08:54 +0800

    style: styled authentication page

--- app/forms.py
+++ app/forms.py
@@ -8,7 +8,7 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 
 class AuthenticationForm(FlaskForm):
-    client_id = StringField('Client ID: ', validators=[DataRequired()])
-    client_secret = PasswordField('Client Secret: ', validators=[DataRequired()])
-    client_url = StringField('Subdomain: ', validators=[DataRequired()])
+    client_id = StringField('Client ID', validators=[DataRequired()])
+    client_secret = PasswordField('Client Secret', validators=[DataRequired()])
+    client_url = StringField('Subdomain', validators=[DataRequired()])
     submit = SubmitField('Authenticate')
